c451e856d6ec6e4303902bf832344fe677f54e52
KeziaWahome <k.wahome@alustudent.com>
2023-03-20 12:47:49 +0200

    With Firebase Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:bmi_calculator/screens/input_page.dart';
-
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_signin/screens/signin_screen.dart';
 void main() => runApp(BMICalculator());
+
 
 class BMICalculator extends StatelessWidget {
   @override
